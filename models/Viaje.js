84c491936bac11bcc882caa867ddfcb7c225adeb
user <user@Users-MacBook-Pro-2.local>
2023-07-10 15:55:51 -0600

    Adding a model

---
+++ models/Viaje.js
@@ -0,0 +1,32 @@
+import { Sequelize } from "sequelize";
+import db from "../config/db";
+
+//DEfinir el modelo
+//define(agregar el nombre de la tabla y un objeto de configuracion donde se definira cada una de las tablas de la db)
+
+export const Viaje = db.define("viajes", {
+  titulo: {
+    type: Sequelize.STRING,
+  },
+  precio: {
+    type: Sequelize.STRING,
+  },
+  fecha_ida: {
+    type: Sequelize.DATE,
+  },
+  fecha_vuelta: {
+    type: Sequelize.DATE,
+  },
+  imagen: {
+    type: Sequelize.STRING,
+  },
+  descripcion: {
+    type: Sequelize.STRING,
+  },
+  disponibles: {
+    type: Sequelize.STRING,
+  },
+  slug: {
+    type: Sequelize.STRING,
+  },
+});
