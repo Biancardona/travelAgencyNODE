4e2e66d7df45352e3b3ef88f3188e4cda4d98c1b
user <user@Users-MacBook-Pro-2.local>
2023-07-18 16:30:19 -0600

    fixing error

--- config/db.js
+++ config/db.js
@@ -9,9 +9,6 @@ const db = new Sequelize(
   process.env.BD_NAME,
   process.env.BD_USER,
   process.env.BD_PASS,
-  process.env.BD_PORT,
-  process.env.BD_HOST,
-
   {
     host: process.env.BD_HOST,
     port: process.env.BD_PORT,
