57286289c6cad2d81f200cfa89aa9afea26198f4
user <user@Users-MacBook-Pro-2.local>
2023-07-18 16:38:15 -0600

    updating environment variables

--- config/db.js
+++ config/db.js
@@ -6,12 +6,12 @@ dotenv.config();
 //New instance of sequelize
 //Pass the name of the db , and the user name, the password(empty), and some configs
 const db = new Sequelize(
-  process.env.BD_NAME,
-  process.env.BD_USER,
-  process.env.BD_PASS,
+  process.env.MYSQLDATABASE,
+  process.env.MYSQLUSER,
+  process.env.MYSQLPASSWORD,
   {
-    host: process.env.BD_HOST,
-    port: process.env.BD_PORT,
+    host: process.env.MYSQLHOST,
+    port: process.env.MYSQLPORT,
     dialect: "mysql",
     define: {
       timestamps: false,
